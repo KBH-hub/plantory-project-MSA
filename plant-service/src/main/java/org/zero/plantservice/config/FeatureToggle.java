1030da3caf2930e954acc7b5993e5590de5943de
soo8848 <soo8848@github.com>
2025-12-27 17:27:32 +0900

    feat: add connect config-server

---
+++ plant-service/src/main/java/org/zero/plantservice/config/FeatureToggle.java
@@ -0,0 +1,14 @@
+package org.zero.plantservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+@RefreshScope
+@Component
+public class FeatureToggle {
+    @Value("${admin.property}")
+    private String value;
+
+    public String getValue() { return value; }
+}
